212ff4bb704d56c1e00788f2b0543b04067ac943
JorgeOsorio97 <jorge.valdez.osorio@gmail.com>
2019-12-03 13:12:48 -0600

    tablas descriptivas de los datos

--- main.R
+++ main.R
@@ -1,7 +1,28 @@
-data <- read.csv("fifa19.csv")
-cuantitative <- subset(data, selecct = c(Value,Overall))
-cualitative <- subset(data, select = c(Work.Rate, Position))
+library(stringr)
 
-cuantitative$Value
-cualitative$Work.Rate
+source("utils.R")
+
+# Tomamos todos los datos del data set
+complete_data <- read.csv("fifa19.csv")
+
+# Tomamos las 4 variable a tarbajar
+# * Cuantitativas:
+#     Values -> Valor en euros del Jugador
+#     Overall -> Calificación General del jugador
+#     AttackRate -> Que tanto participa en jugadas de ataque
+#     Position -> Posicion en la que juega 
+data <- data.frame(
+  Value = unlist(lapply(as.list(complete_data$Value), cleanPlayerValue)),
+  Overall = complete_data$Overall,
+  AttackRate = str_split_fixed(complete_data$Work.Rate, "/ ", 2)[,1],
+  Position = unlist(lapply(as.list(complete_data$Position, getTwoMaxValues(complete_data$Position)), cleanPlayerPosition))
+)
+
+# Eliminamos valores 0 de Value
+data <- data[data$Value>0,]
+
+# Creamos tabla de resumen de ambas variables cuantitativas
+cuantitatives <- data.frame(data$Value, data$Overall)
+resume_cuantitatives <- summary(cuantitatives)
+resume_cuantitatives <- rbind(resume_cuantitatives, c(paste("Sd." ,sd(cuantitatives$data.Value)), paste("Sd." ,sd(cuantitatives$data.Overall))))
 
---
+++ utils.R
@@ -0,0 +1,29 @@
+
+cleanPlayerValue <- function(original_value){
+  original_value = toString(original_value)
+  #print(original_value)
+  mult <- 0
+  last_digit <- substr(original_value ,nchar(original_value), nchar(original_value))
+  if(last_digit=="K"){
+    mult <- 1000
+  }
+  if(last_digit=="M"){
+    mult <- 1000000
+  }
+  if(last_digit==0){
+    return(0)
+  }
+  return(as.numeric(substr(original_value, 2, nchar(original_value)-1)) * mult)
+}
+
+cleanPlayerPosition <- function(original_position, main_vals=c("ST","GK")){
+  if(original_position %in% main_vals){
+    return(toString(original_position))
+  } else{
+    return("Other")
+  }
+}
+
+getTwoMaxValues <- function (list){
+  c(names(sort(table(list),decreasing=TRUE)[1:2]))
+}
